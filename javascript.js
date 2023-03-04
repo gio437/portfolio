8e4a12a10745fbaafea367d94071b5e82158b528
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-03-04 18:35:24 -0500

    adds background to about me text, adds weather app project

--- javascript.js
+++ javascript.js
@@ -2,17 +2,18 @@ const aboutNav = document.querySelector('.about-side');
 const cvNav = document.querySelector('.cv-side');
 const battleNav= document.querySelector('.battle-side');
 const ticNav = document.querySelector('.tic-side');
+const weatherNav = document.querySelector('.weather-side');
 const contactNav = document.querySelector('.contact-side');
 
 aboutNav.addEventListener('click', () => {
-    // aboutNav.style.color = 'white';
-    // aboutNav.style.backgroundColor = 'darkgreen';;
-    // cvNav.style.color = '#696969';
-    // cvNav.style.backgroundColor = '#1b1b1b';
-    // battleNav.style.color = '#696969';
-    // battleNav.style.backgroundColor = '#1b1b1b';
-    // ticNav.style.color = '#696969';
-    // ticNav.style.backgroundColor = '#1b1b1b';
+      // aboutNav.style.color = 'white';
+      // aboutNav.style.backgroundColor = 'darkgreen';;
+      // cvNav.style.color = '#696969';
+      // cvNav.style.backgroundColor = '#1b1b1b';
+      // battleNav.style.color = '#696969';
+      // battleNav.style.backgroundColor = '#1b1b1b';
+      // ticNav.style.color = '#696969';
+      // ticNav.style.backgroundColor = '#1b1b1b';
 
     window.scroll({
         top: 0,
@@ -67,6 +68,13 @@ ticNav.addEventListener('click', () => {
         behavior: 'smooth'
       });
 });
+
+weatherNav.addEventListener('click', () => {
+  window.scroll({
+      top: 2500,
+      behavior: 'smooth'
+    });
+})
 
 contactNav.addEventListener('click', () => {
     window.scroll({
